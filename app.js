b83e2cb1170a8631e3ff3737724b1cb787e8ba3a
Ganesh1724 <yashwanthganesh123@gmail.com>
2024-08-25 17:29:16 +0530

    Hii How are you

---
+++ app.js
@@ -0,0 +1,16 @@
+const normal = document.querySelector("ul");
+
+console.log(normal);
+
+console.log(normal.className);
+
+
+console.log(normal.classList);
+
+// normal.className.add("new-list");
+
+// normal.classList.remove("list");
+
+
+
+
